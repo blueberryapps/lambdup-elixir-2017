16b5d3ddfaa794a6869d52059351434fdaf1260d
lupuszr <vikpelle@gmail.com>
2017-12-04 22:18:28 +0100

    Add genserver implementation

--- lib/elixir_workshop_2017/redit/api_genserver.ex
+++ lib/elixir_workshop_2017/redit/api_genserver.ex
@@ -1,23 +1,55 @@
 defmodule ElixirWorkshop2017.Redit.ApiGenServer do
   use GenServer
+  alias ElixirWorkshop2017.Utils.Http.Request
 
   ## API
   def start_link(_) do
-    GenServer.start_link(__MODULE__, nil, [])
+    GenServer.start_link(__MODULE__, nil, [name: :api_gen_server])
   end
 
   def init(_) do
     {:ok, nil}
   end
 
-  def get_subreddits(query) do
-    GenServer.call(__MODULE__, {:fetch, query})
+  def get_available_subreddits(query) do
+    GenServer.call(:api_gen_server, {:fetch_available_subreddits, query})
+  end
+
+  def get_reddit_posts(subreddit) do
+    GenServer.call(:api_gen_server, {:fetch_reddit, subreddit})
   end
 
   ## Callbacks
 
-  def handle_call({:fetch, query}, _from, state) do
-    IO.inspect "called with #{query}"
+  def handle_call({:fetch_available_subreddits, query}, _from, state) do
+    computed_url = "https://www.reddit.com/api/subreddits_by_topic.json?query=#{query}"
+    
+    with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <- Request.get(computed_url),
+      {:ok, decoded_json} <- Poison.decode(body),
+      data <- Enum.map(decoded_json, &(&1["name"]))
+    do
+      {:reply, data, data}
+    else
+      {:error, %HTTPoison.Error{reason: reason}} -> {:error, reason}
+    end
+  end
+
+  def handle_call({:fetch_reddit, subreddit}, _from, state) do
+    computed_url = "https://www.reddit.com/r/#{subreddit}.json"
+
+    with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <- Request.get(computed_url),
+      {:ok, decoded_json} <- Poison.decode(body),
+      data <- Enum.map(decoded_json["data"]["children"], &(%{
+        title: &1["data"]["title"],
+        created: &1["data"]["created"],
+        author: &1["data"]["author"],
+        link: &1["data"]["permalink"]
+      }))
+    do
+      {:reply, data, data}
+    else
+      {:error, reason} -> {:reply, :error, reason}
+    end
   end
 end
 
