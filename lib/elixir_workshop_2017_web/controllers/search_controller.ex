f4742cb49189c9a18555fc4048dffc09621c20ee
lupuszr <vikpelle@gmail.com>
2017-12-05 09:10:34 +0100

    Display results

--- lib/elixir_workshop_2017_web/controllers/search_controller.ex
+++ lib/elixir_workshop_2017_web/controllers/search_controller.ex
@@ -3,25 +3,28 @@ defmodule ElixirWorkshop2017Web.SearchController do
 
   alias ElixirWorkshop2017.Redit
   alias ElixirWorkshop2017.Redit.Search
+  alias ElixirWorkshop2017.Redit.ApiGenServer
 
   def index(conn, _params) do
     searches = Redit.list_searches()
     render(conn, "index.html", searches: searches)
   end
 
-  def new(conn, _params) do
-    changeset = Redit.change_search(%Search{})
-    render(conn, "new.html", changeset: changeset)
+  def new(conn, %{"search" => %{"query" => query}}) do
+    topics = ApiGenServer.get_available_subreddits(query)
+    render(conn, "new.html", topics: topics)
   end
 
-  def create(conn, %{"search" => search_params}) do
-    case Redit.create_search(search_params) do
-      {:ok, search} ->
-        conn
-        |> put_flash(:info, "Search created successfully.")
-        |> redirect(to: search_path(conn, :show, search))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
+  def create(conn, params) do
+    selected_topics = Enum.filter(params, &(elem(&1,1) == "true")) |> Enum.map(&(elem(&1, 0)))
+    grouped_posts = selected_topics |> Enum.map(fn(topic) -> {topic, ApiGenServer.get_reddit_posts(topic)} end) 
+    
+    conn
+    |> render("show.html", grouped_posts: grouped_posts)
+
+    # TODO: save it to DB
+    # conn
+    # |> put_flash(:info, "Search created successfully.")
+    # |> redirect(to: search_path(conn, :show, 1, posts))
   end
 end
